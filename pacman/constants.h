dea58d0270def3d23aed40eca6cbfad9deb953da
paulo-blips <paulo.silva@blips.com>
2026-07-14 07:39:52 -0300

    feat: jogo da adivinhação em c++, entrada e saída de valores, estruturas de repetição, condicionais e números reais.

---
+++ pacman/constants.h
@@ -0,0 +1,29 @@
+struct map {
+    char** matriz;
+    int linhas;
+    int colunas;
+};
+
+struct pos {
+    int x;
+    int y;
+};
+
+typedef struct map MAPA;
+typedef struct pos POSICAO;
+
+void pillExplode2(int x, int y, int somax, int somay, int qnt);
+void pillExplode();
+
+#define FANTASMA 'F'
+#define HEROI '@'
+#define PILULA 'P'
+#define BOMBA 'b'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+#define CAMINHO '.'
+#define CIMA 'w'
+#define BAIXO 's'
+#define ESQUERDA 'a'
+#define DIREITA 'd'
+
